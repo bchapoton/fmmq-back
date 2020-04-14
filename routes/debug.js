9cc144095e4ebf2195ecce5dd150d9245e9bffaa
Benjamin CHAPOTON <b.chapoton@gmail.com>
2020-04-14 15:36:00 +0400

    string compare

--- routes/debug.js
+++ routes/debug.js
@@ -5,23 +5,122 @@ const socket = require('../socket/FMMQSocket');
 const {createSocketRoom} = require("../socket/SocketHelper");
 const gameCache = require('../services/CacheService');
 
-router.get('/test', function(req, res, next) {
-  socket.emit('try', 'from endpoint');
-  socket.of('currentroom').emit('try', 'current room event');
-  socket.of('room1').emit('try', 'room1 event');
-  socket.of('room2').emit('try', 'room2 event');
-  res.send();
+router.get('/test', function (req, res, next) {
+    socket.emit('try', 'from endpoint');
+    socket.of('currentroom').emit('try', 'current room event');
+    socket.of('room1').emit('try', 'room1 event');
+    socket.of('room2').emit('try', 'room2 event');
+    res.send();
 });
 
-router.get('/game/:categoryId/delete', function(req, res, next) {
-  const categoryId = req.params.categoryId;
-  gameCache.delete(categoryId);
-  res.send();
+router.get('/game/:categoryId/delete', function (req, res, next) {
+    const categoryId = req.params.categoryId;
+    gameCache.delete(categoryId);
+    res.send();
 });
 
-router.get('/init2', function(req, res, next) {
-  createSocketRoom(socket, 'room2');
-  res.send();
+router.get('/init2', function (req, res, next) {
+    createSocketRoom(socket, 'room2');
+    res.send();
+});
+
+const accents = require('remove-accents');
+
+const testRemove = (str) => {
+    let start = new Date().getTime();
+    const removed = accents.remove(str);
+    console.log('time : ' + (new Date().getTime() - start));
+    console.log('original :' + str);
+    console.log('removed  :' + removed);
+}
+
+router.get('/accents', function (req, res, next) {
+    testRemove("éèàùëêîïôöuûü");
+    testRemove("u tést à voïrÔ IÎïÏ");
+    testRemove("un texte sans accents pour voir comment ça se passe");
+    testRemove("ÀÁÂÃÄÅàáâãäåÒÓÔÕÕÖØòóôõöøÈÉÊËèéêëðÇçÐÌÍÎÏìíîïÙÚÛÜùúûüÑñŠšŸÿýŽž");
+});
+
+const compareAlgo = require("damerau-levenshtein-js");
+
+const compare = (str1, str2) => {
+    let start = new Date().getTime();
+    /*
+    const str1Split = str1.split(' ');
+    const str2Split = str2.split(' ');
+    */
+
+    const distance = compareAlgo.distance(str1, str2);
+    console.log('time : ' + (new Date().getTime() - start));
+    console.log('string : ' + str1);
+    console.log('string : ' + str2);
+    console.log('distance : ' + distance);
+};
+
+
+const compare2 = (originalString, guessTryStr) => {
+    console.log('------------------------------------------')
+    let start = new Date().getTime();
+    // allow more error in long word
+    const smallWordAllowedDistance = [0, 1];
+    const longWordAllowedDistance = [0, 1, 2];
+
+    // clean the strings remove accent and set to lower case
+    const sanitizedOriginalStr = accents.remove(originalString).toLowerCase();
+    const sanitizedGuessTry = accents.remove(guessTryStr).toLowerCase();
+
+    // split each words in the strings to compare word to word
+    const originalStrSplit = sanitizedOriginalStr.split(' ');
+    const guessTrySplit = sanitizedGuessTry.split(' ');
+    const found = [];
+
+    // copy original array, we will remove on each searched all found elements
+    let internalGuessTrySplit = [...guessTrySplit];
+    originalStrSplit.forEach(originalWord => {
+        const internalOriginalFound = [];
+        const internalGuessFound = [];
+        internalGuessTrySplit.forEach(guessWord => {
+            const distance = compareAlgo.distance(originalWord, guessWord);
+            let isFound;
+            if (originalWord.length < 5) {
+                isFound = smallWordAllowedDistance.includes(distance)
+            } else {
+                isFound = longWordAllowedDistance.includes(distance);
+            }
+            if (isFound) {
+                internalOriginalFound.push(originalWord);
+                internalGuessFound.push(guessWord);
+            }
+        });
+        found.push(internalOriginalFound);
+        // remove all the found words in this iteration
+        internalGuessTrySplit = internalGuessTrySplit.filter(item => !internalGuessFound.includes(item));
+        console.log('iteration guess try split : ' + internalGuessTrySplit);
+    });
+    console.log('time : ' + (new Date().getTime() - start));
+    console.log('string : ' + originalString);
+    console.log('san    : ' + sanitizedOriginalStr);
+    console.log('string : ' + guessTryStr);
+    console.log('san    : ' + sanitizedGuessTry);
+    console.log('found : ' + found);
+};
+
+router.get('/compare', function (req, res, next) {
+    /*
+    compare('oops','ops');
+    compare('oops','ooops');
+    compare('oops','opos');
+    compare('oops','op');
+    compare('oops','y');
+    */
+    compare2('oops i did it again', 'ops i dit it agen');
+    compare2('oops i did it again', 'oops i did it agai');
+    compare2('oops i did it again', 'op did it again');
+    compare2('oops i did it again', 'op did aga');
+
+    compare2('La vallée de Dana', 'la valee de dan');
+
+    res.send();
 });
 
 module.exports = router;
--- services/GameService.js
+++ services/GameService.js
@@ -5,6 +5,8 @@ const {Category} = require("../models/category.model");
 const {pickMusics} = require("./MusicService");
 const {Room} = require("./bean/Room");
 const {emitOnEnter} = require("./EventEmitterService");
+const accents = require('remove-accents');
+const compareAlgo = require("damerau-levenshtein-js");
 
 const getCurrentRoomPlayers = (categoryId) => {
     const game = cacheService.get(categoryId);
@@ -88,6 +90,67 @@ const getPlayerFromUserContext = (req) => {
     return null;
 };
 
+/**
+ * Remove accents and set to lower case
+ *
+ * @param string element the title or artist
+ * @return string the accents free and lower case string
+ */
+const sanitizeMusicElement = (element) => {
+    return accents.remove(element).toLowerCase();
+};
+
+const splitMusicElement = (element) => {
+    return element.split(' ');
+};
+
+/**
+ * Compare sanitizedGuessTry
+ *
+ * @param sanitizedOriginalStr the original string without accents and uppercase
+ * @param sanitizedGuessTry the guess try array without accents and uppercase already split by the function splitMusicElement
+ * @return {{originalWordFound: [], guessWordFound: []}} original words found in this round and the guess try found in this round
+ */
+const compareGuessTry = (sanitizedOriginalStr, sanitizedGuessTrySplit) => {
+    // allow more error in long word
+    const smallWordAllowedDistance = [0, 1];
+    const longWordAllowedDistance = [0, 1, 2];
+
+    // split each words in the strings to compare word to word
+    const originalStrSplit = splitMusicElement(sanitizedOriginalStr);
+    const originalWordFound = [];
+    const guessWordFound = [];
+    // copy original array, we will remove on each searched all found elements
+    let internalGuessTrySplit = [...sanitizedGuessTrySplit];
+
+    originalStrSplit.forEach(originalWord => {
+        const internalOriginalFound = [];
+        const internalGuessFound = [];
+        internalGuessTrySplit.forEach(guessWord => {
+            const distance = compareAlgo.distance(originalWord, guessWord);
+            let isFound;
+            if (originalWord.length < 5) {
+                isFound = smallWordAllowedDistance.includes(distance)
+            } else {
+                isFound = longWordAllowedDistance.includes(distance);
+            }
+            if (isFound) {
+                internalOriginalFound.push(originalWord);
+                internalGuessFound.push(guessWord);
+            }
+        });
+        originalWordFound.push(internalOriginalFound);
+        guessWordFound.push(internalGuessFound);
+        // remove all the found words in this iteration
+        internalGuessTrySplit = internalGuessTrySplit.filter(item => !internalGuessFound.includes(item));
+    });
+
+    return {originalWordFound: originalWordFound, guessWordFound: guessWordFound};
+};
+
 exports.getCurrentRoomPlayers = getCurrentRoomPlayers;
 exports.enterRoom = enterRoom;
 exports.getPlayerFromUserContext = getPlayerFromUserContext;
+exports.sanitizeMusicElement = sanitizeMusicElement;
+exports.splitMusicElement = splitMusicElement;
+exports.compareGuessTry = compareGuessTry;
